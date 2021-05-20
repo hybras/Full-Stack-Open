f76b8bfefc22512f360a2d1844c96ded4aa35a5c
hybras <24651269+hybras@users.noreply.github.com>
2021-05-20 14:57:16 -0400

    1.3

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -4,8 +4,8 @@ const Header = (props: { course: String }) => (<div>
   <h1>{props.course}</h1>
 </div>)
 
-const Part = (props: { part: String, exercise: number }) => (
-  <p>{props.part} {props.exercise}</p>
+const Part = (props: { name: String, exercise: number }) => (
+  <p>{props.name} {props.exercise}</p>
 )
 
 const Footer = (props: { total_exercises: number }) => (
@@ -15,20 +15,26 @@ const Footer = (props: { total_exercises: number }) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Part part={part1} exercise={exercises1} />
-      <Part part={part2} exercise={exercises2} />
-      <Part part={part3} exercise={exercises3} />
-      <Footer total_exercises={exercises1 + exercises2 + exercises3}></Footer>
+      <Part name={part1.name} exercise={part1.exercises} />
+      <Part name={part2.name} exercise={part2.exercises} />
+      <Part name={part3.name} exercise={part3.exercises} />
+      <Footer total_exercises={part1.exercises + part2.exercises + part3.exercises}></Footer>
 
     </div>
   )
