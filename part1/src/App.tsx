9cec1087417e2e883a5f253638c4dc2ee9bfc242
hybras <24651269+hybras@users.noreply.github.com>
2021-05-20 16:37:54 -0400

    1.5

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -1,49 +1,59 @@
 import React from 'react'
 
-const Header = (props: { course: String }) => (<div>
-  <h1>{props.course}</h1>
+
+class Course {
+  name: String = ""
+  parts: { name: String, exercises: number }[] = []
+}
+
+const Header = (props: { course: Course }) => (<div>
+  <h1>{props.course.name}</h1>
 </div>)
 
 const Part = (props: { name: String, exercise: number }) => (
   <p>{props.name} {props.exercise}</p>
 )
 
-const Footer = (props: { total_exercises: number }) => (
-  <p>Number of exercises {props.total_exercises}</p>
+const Footer = (props: { course: Course }) => (
+  <p>Number of exercises {props.course.parts.map(it => it.exercises).reduce((acc, it) => acc + it, 0)}</p>
 )
 
 
 
-const Content = (props: { parts: { name: String, exercises: number }[] }) =>
-(<div>
-  <Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
-  <Part name={props.parts[1].name} exercise={props.parts[1].exercises} />
-  <Part name={props.parts[2].name} exercise={props.parts[2].exercises} />
-</div>)
+const Content = (props: { course: Course }) => {
+  const parts = props.course.parts
+  return (<div>
+    <Part name={parts[0].name} exercise={parts[0].exercises} />
+    <Part name={parts[1].name} exercise={parts[1].exercises} />
+    <Part name={parts[2].name} exercise={parts[2].exercises} />
+  </div>)
+}
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Footer total_exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Content course={course} />
+      <Footer course={course} />
 
     </div>
   )
