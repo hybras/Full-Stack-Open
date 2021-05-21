1741a28d19049075731b12f350462a06d113e53d
hybras <24651269+hybras@users.noreply.github.com>
2021-05-21 15:01:41 -0400

    Start unicafe

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,53 +1,13 @@
-import React from 'react'
-
-const Header = ({ course }) => (<div>
-  <h1>{course.name}</h1>
-</div>)
-
-const Part = ({ name, exercise }) => (
-  <p>{name} {exercise}</p>
-)
-
-const Footer = ({ course }) => (
-  <p>Number of exercises {course.parts.map(it => it.exercises).reduce((acc, it) => acc + it, 0)}</p>
-)
-
-
-
-const Content = ({ course }) => {
-  const parts = course.parts
-  return (<div>
-    <Part name={parts[0].name} exercise={parts[0].exercises} />
-    <Part name={parts[1].name} exercise={parts[1].exercises} />
-    <Part name={parts[2].name} exercise={parts[2].exercises} />
-  </div>)
-}
-
+import React, { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Footer course={course} />
     </div>
   )
 }
