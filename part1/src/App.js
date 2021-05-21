29d762b5f92457556b7cbc6f82cfe1f484d2a62a
hybras <24651269+hybras@users.noreply.github.com>
2021-05-21 15:15:30 -0400

    1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,24 @@
 import React, { useState } from 'react'
 
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const GiveFeedback = ({ onClicks }) => (<div>
+  <h1>Give Feedback</h1>
+  <Button text="good" onClick={onClicks[0]} />
+  <Button text="neutral" onClick={onClicks[1]} />
+  <Button text="bad" onClick={onClicks[2]} />
+</div>)
+
+const Statistics = ({ good, bad, neutral }) => (<div>
+  <p>Good: {good}</p>
+  <p>Neutral: {neutral}</p>
+  <p>Bad: {bad}</p>
+</div>)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,6 +27,8 @@ const App = () => {
 
   return (
     <div>
+      <GiveFeedback onClicks={[() => setGood(good + 1), () => setNeutral(neutral + 1), () => setBad(bad + 1)]} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
