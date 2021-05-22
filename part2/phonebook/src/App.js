acfedd2d51b184fa5a1981de474aed76a8b90c73
hybras <24651269+hybras@users.noreply.github.com>
2021-05-22 17:31:59 -0400

    Add dummy data

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -7,10 +7,16 @@ import People from './components/People';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '666 666 6666' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
 
-  return (<div><AddPerson persons={persons} setPersons={setPersons} /><People persons={persons}/></div>)
+  return (<div>
+    <AddPerson persons={persons} setPersons={setPersons} />
+    <People persons={persons} />
+  </div>)
 }
 
 export default App
