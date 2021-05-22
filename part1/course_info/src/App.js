ddf646967ad872dd45f308ac65bad44f4e719ff3
hybras <24651269+hybras@users.noreply.github.com>
2021-05-22 13:27:30 -0400

    2.5

--- part1/course_info/src/App.js
+++ part1/course_info/src/App.js
@@ -1,32 +1,5 @@
 import React from 'react'
-
-const Header = ({ course }) => (<div>
-  <h2>{course.name}</h2>
-</div>)
-
-const Part = ({ name, exercise }) => (
-  <p>{name} {exercise}</p>
-)
-
-const Footer = ({ course }) => (
-  <p>Number of exercises {course.parts.map(it => it.exercises).reduce((acc, it) => acc + it, 0)}</p>
-)
-
-const Content = ({ course }) => {
-  const parts = course.parts
-  return (<div>
-    {parts.map(it => <Part key={it.id} name={it.name} exercise={it.exercises} />)}
-  </div>)
-}
-
-const Course = ({ course }) => (
-  <div>
-    <Header course={course} />
-    <Content course={course} />
-    <Footer course={course} />
-  </div>
-)
-
+import Course from './Course'
 
 const App = () => {
   const courses = [
