59c0f17cb75064f60aff6382154dc4de3247518c
hybras <24651269+hybras@users.noreply.github.com>
2021-05-22 13:16:14 -0400

    2.{1,2,3}

--- part1/course_info/src/App.js
+++ part1/course_info/src/App.js
@@ -12,44 +12,46 @@ const Footer = ({ course }) => (
   <p>Number of exercises {course.parts.map(it => it.exercises).reduce((acc, it) => acc + it, 0)}</p>
 )
 
-
-
 const Content = ({ course }) => {
   const parts = course.parts
   return (<div>
-    <Part name={parts[0].name} exercise={parts[0].exercises} />
-    <Part name={parts[1].name} exercise={parts[1].exercises} />
-    <Part name={parts[2].name} exercise={parts[2].exercises} />
+    {parts.map(it => <Part key={it.id} name={it.name} exercise={it.exercises} />)}
   </div>)
 }
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Footer course={course} />
+  </div>
+)
 
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Footer course={course} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
