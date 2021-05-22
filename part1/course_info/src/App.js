86b1e16dc8fce07e469424e1db558c24de3e7025
hybras <24651269+hybras@users.noreply.github.com>
2021-05-22 13:23:44 -0400

    2.4

--- part1/course_info/src/App.js
+++ part1/course_info/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = ({ course }) => (<div>
-  <h1>{course.name}</h1>
+  <h2>{course.name}</h2>
 </div>)
 
 const Part = ({ name, exercise }) => (
@@ -29,29 +29,55 @@ const Course = ({ course }) => (
 
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (<div>
+    <h1>Wed Dev Curriculum</h1>
+    {courses.map(it => <Course key={it.id} course={it} />)}
+  </div>)
 }
 
 export default App
