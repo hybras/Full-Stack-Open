a20e638369a13debb102efeff4169d5666f61b53
hybras <24651269+hybras@users.noreply.github.com>
2021-05-22 19:42:21 -0400

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AddPerson from './components/AddPerson';
 import People from './components/People';
 import Search from './components/Search';
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(resp => { setPersons(resp.data) })
+  }, [])
 
   return (<div>
     <AddPerson persons={persons} setPersons={setPersons} />
     <Search search={search} setSearch={setSearch} />
-    <People persons={persons} search={search}/>
+    <People persons={persons} search={search} />
   </div>)
 }
 
