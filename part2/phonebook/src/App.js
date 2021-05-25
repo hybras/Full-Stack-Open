58d619b7b5b35d22b9f2ee0507d94adb97cd9627
hybras <24651269+hybras@users.noreply.github.com>
2021-05-25 15:22:58 -0400

    Properly get id for newly added people
    I previously forgot to use using promise chaining

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,6 +9,12 @@ const App = () => {
   const [persons, setPersons] = useState([])
   const [search, setSearch] = useState('')
 
+  const addPerson = person => {
+    Phonebook.create(person).then(new_person => {
+      console.log(new_person)
+      const new_persons = persons.concat(new_person)
+      setPersons(new_persons)})
+  }
 
   const updatePerson = person => {
     const id = persons.find(it => it.name === person.name).id
@@ -23,7 +29,7 @@ const App = () => {
   }, [])
 
   return (<div>
-    <AddPerson persons={persons} addPerson={addPerson} />
+    <AddPerson persons={persons} addPerson={addPerson} updatePerson={updatePerson} />
     <Search search={search} setSearch={setSearch} />
     <People persons={persons} search={search} setPersons={setPersons} />
   </div>)
