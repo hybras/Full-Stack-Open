d4e1336f2b859d812b74669f1b4e5eaba8c300c5
hybras <24651269+hybras@users.noreply.github.com>
2021-05-27 19:56:10 -0400

    3.5

--- part3/ph_backend/src/index.js
+++ part3/ph_backend/src/index.js
@@ -1,26 +1,30 @@
 const express = require('express')
+const { nanoid } = require('nanoid')
 const app = express()
+
+app.use(express.json())
+
 
 let persons = [
     {
         "name": "Arto Hellas",
         "number": "040-123456",
-        "id": 1
+        "id": nanoid()
     },
     {
         "name": "Ada Lovelace",
         "number": "39-44-5323523",
-        "id": 2
+        "id": nanoid()
     },
     {
         "name": "Dan Abramov",
         "number": "12-43-234345",
-        "id": 3
+        "id": nanoid()
     },
     {
         "name": "Mary Poppendieck",
         "number": "39-23-6423122",
-        "id": 4
+        "id": nanoid()
     }
 ]
 
@@ -29,7 +33,7 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
+    const id = String(request.params.id)
     const person = persons.find(it => it.id === id)
     if (person) {
         response.json(person)
@@ -40,7 +44,7 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
+    const id = String(request.params.id)
     const person = persons.find(it => it.id === id)
     const new_persons = persons.filter(it => it.id !== id)
     if (person) {
@@ -50,6 +54,15 @@ app.delete('/api/persons/:id', (request, response) => {
     else {
         response.status(204).end()
     }
+})
+
+app.post('/api/persons', (req, res) => {
+    const perp = req.body
+    const id = nanoid()
+    const person = { id, ...perp }
+    console.log('Person: ', perp)
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 app.get('/info', (req, resp) => {
