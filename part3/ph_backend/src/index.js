c7792b4cf1c32176085d403499f44fb1f2bcc98e
hybras <24651269+hybras@users.noreply.github.com>
2021-05-27 23:28:35 -0400

    Statically serve frontend

--- part3/ph_backend/src/index.js
+++ part3/ph_backend/src/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 const app = express()
 
-// app.use(express.static('build'))
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 morgan.token('body', (req) => {
