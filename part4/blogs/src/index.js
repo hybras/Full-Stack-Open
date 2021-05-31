d23f20e03cdc1931e6f26aa824c245936124d7eb
hybras <24651269+hybras@users.noreply.github.com>
2021-05-31 15:37:24 -0400

    Refactor blog model

--- part4/blogs/src/index.js
+++ part4/blogs/src/index.js
@@ -2,16 +2,6 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = 'mongodb://localhost/bloglist'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
