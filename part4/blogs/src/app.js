6e45e6aab25ada9c8cf63d4583fc8e3e6edc6dc8
hybras <24651269+hybras@users.noreply.github.com>
2021-05-31 15:48:16 -0400

    Refactor app and routes

---
+++ part4/blogs/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+const routes = require('./controllers/blogs')
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', routes)
+
+module.exports = app
