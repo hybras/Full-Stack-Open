90c698712123e8450dd3f1e19dae1b50ff9a9580
hybras <24651269+hybras@users.noreply.github.com>
2021-06-06 10:06:55 -0400

    4.4

--- part4/blogs/src/utils/list_helper.js
+++ part4/blogs/src/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.map(it => it.likes).reduce((acc, it) => acc + it, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
