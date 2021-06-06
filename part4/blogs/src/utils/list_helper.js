be7726e3b636e93b6e1575210e3547b980601052
hybras <24651269+hybras@users.noreply.github.com>
2021-06-06 10:53:24 -0400

    4.5

--- part4/blogs/src/utils/list_helper.js
+++ part4/blogs/src/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.map(it => it.likes).reduce((acc, it) => acc + it, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const sortedBlogs = [...blogs]
+  sortedBlogs.sort((a, b) => a.likes - b.likes)
+  const blog = sortedBlogs.slice(-1)[0]
+  return {
+    likes: blog.likes,
+    author: blog.author,
+    title: blog.title
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
